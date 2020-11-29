96b84272505387a91453bbb845314297a5d4297d
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-11-29 10:15:29 +0530

    Update start.py

--- start.py
+++ start.py
@@ -35,7 +35,7 @@ async def handler(event):
     r1 = requests.get(urls.search_api_endpoint + w,headers=headers, params={"translation":"en", "country":"IN"}).json()
     g1 = (r1["hls"][0].replace("drm", "hls") + req1["video_token"])
    # await client.send_file(chat,r1["image_url"],caption = r1["title"])
-    markup = client.build_reply_markup(Button.url("Zee5 Stream",urls.stream_baseurl+g1))
+    markup = client.build_reply_markup(Button.url("Zee5_Stream",urls.stream_baseurl+g1))
     await client.send_message(chat, "Support @SerialCoIn & @urlicupload\n\n"+"🎥 "+r1["title"]+"\n\n📑 "+r1["description"],file=r1["image_url"], buttons=markup)   
     
     #s = requests.Session()
@@ -59,8 +59,8 @@ async def handler(event):
     video_d = "https://llvod.mxplay.com/"
     A =requests.get("https://api.mxplay.com/v1/web/detail/video?type=movie&id="+link+"&platform=com.mxplay.desktop&device-density=2&userid=30bb09af-733a-413b-b8b7-b10348ec2b3d&platform=com.mxplay.mobile&content-languages=hi,en,ta").json()
     chat = await event.get_chat()
-    markup = client.build_reply_markup(Button.url("Mx Stream",video_d+A["stream"]['hls']['high']))
-    await client.send_message(chat," support @urlicupload   "+A["title"],buttons=markup)
+    markup = client.build_reply_markup(Button.url("Mx_Player_Stream",video_d+A["stream"]['hls']['high']))
+    await client.send_message(chat," Support @SerialCoIn & @urlicupload   "+A["title"],buttons=markup)
     print(A)
     print(link)
 @client.on(events.NewMessage(pattern='(?i)https://www.hotstar.com/in/'))
