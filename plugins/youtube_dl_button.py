65f589817db26c9c29555e646fd9bad5a8a29395
AsuranJ <71362075+AsuranJ@users.noreply.github.com>
2020-12-15 02:50:32 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -200,11 +200,11 @@ async def youtube_dl_call_back(bot, update):
               #  1
            # )
             #logger.info(images)
-          #  await bot.edit_message_text(
-              #  text=Translation.UPLOAD_START,
-               # chat_id=update.message.chat.id,
-              #  message_id=update.message.message_id
-          #  )
+            await bot.edit_message_text(
+                text=Translation.UPLOAD_START,
+                chat_id=update.message.chat.id,
+                message_id=update.message.message_id
+            )
             # get the correct width, height, and duration for videos greater than 10MB
             # ref: message from @BotSupport
             width = 0
