777e3f503ca7079db92e9f59b0bb63b005687aec
Nirmalraj10567 <55886074+Nirmalraj10567@users.noreply.github.com>
2020-12-18 23:18:23 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -56,6 +56,7 @@ async def youtube_dl_call_back(bot, update):
         )
         return False
     youtube_dl_url = update.message.reply_to_message.text
+    print (youtube_dl_url)
     custom_file_name = str(response_json.get("title")) + \
         "_" + youtube_dl_format + "." + youtube_dl_ext
     youtube_dl_username = None
